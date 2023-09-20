53104dcd35fbab658be983efe89eece66696db3e
littleBitsman <67122660+littleBitsman@users.noreply.github.com>
2023-09-20 00:06:03 +0000

    added luau for dtr-moderation example

--- examples/discord-to-roblox-moderation-system/main.lua
+++ examples/discord-to-roblox-moderation-system/main.lua
@@ -1,1 +1,17 @@
+local serverUrl = "http://your-url-here.com"
+
 local RTC = require(14549265835) -- docs are somewhere
+
+RTC:StartConnection(serverUrl)
+
+RTC.event("message"):Connect(function(data) { -- data is a dictionary of JSONDecode()'d data that was sent by the backend
+    if data.action == "kick" then
+        -- example kick logic:
+        local player = game.Players:GetPlayerByUserId(data.userId)
+        if player then player:Kick(data.reason) end
+    elseif data.action == "ban" then
+        -- your ban logic here...
+    elseif data.action == "unban" then
+        -- your unban logic here...
+    end
+})
